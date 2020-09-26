24f76cf979603f3c1c0e34e3cb9408c1fb290f2c
miracle-halation <miraclehalation.9710@gmail.com>
2020-09-26 12:09:31 +0900

    adminでログインしたときにアクセスするページを用意

---
+++ pages/admin.vue
@@ -0,0 +1,81 @@
+<template>
+	<v-container>
+		<v-row>
+			<v-col>
+				<v-card
+					class="mx-auto"
+					max-width="344"
+				>
+					<v-card-text>
+						<p class="display-1 text--primary">
+							ラッキーパズル
+						</p>
+					</v-card-text>
+					<v-card-actions>
+						<v-btn text color="orange">
+							編集する
+						</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-col>
+
+			<v-col>
+				<v-card
+					class="mx-auto"
+					max-width="344"
+				>
+					<v-card-text>
+						<p class="display-1 text--primary">
+							お買い物ゲーム
+						</p>
+					</v-card-text>
+					<v-card-actions>
+						<v-btn text color="orange">
+							編集する
+						</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-col>
+
+			<v-col>
+				<v-card
+					class="mx-auto"
+					max-width="344"
+				>
+					<v-card-text>
+						<p class="display-1 text--primary">
+							漢字クイズ
+						</p>
+					</v-card-text>
+					<v-card-actions>
+						<v-btn text color="orange">
+							編集する
+						</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-col>
+
+			<v-col>
+				<v-card
+					class="mx-auto"
+					max-width="344"
+				>
+					<v-card-text>
+						<p class="display-1 text--primary">
+							都道府県クイズ
+						</p>
+					</v-card-text>
+					<v-card-actions>
+						<v-btn text color="orange">
+							編集する
+						</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-col>
+
+		</v-row>
+	</v-container>
+</template>
+
+<script>
+</script>
