1dadbe47d469fac71e98198964e372cbcc00a45c
miracle-halation <miraclehalation.9710@gmail.com>
2020-10-08 17:05:11 +0900

    PuzzleListコンポーネントを作成

--- pages/puzzle/index.vue
+++ pages/puzzle/index.vue
@@ -1,64 +1,15 @@
 <template>
-	<v-container>
-		<h1 class="title">ラッキーパズル</h1>
-		<v-row
-			class="d-flex"
-			justify="center"
-		>
-			<v-menu
-				v-model="showMenu"
-				absolute
-				offset-y
-				style="max-width: 600px"
-			>
-				<template v-slot:activator="{ on, attrs }">
-					<div class="puzzles">
-						<h2>タングラム</h2>
-						<v-card
-							class="portrait"
-							:img="require('@/assets/tangram.png')"
-							height="300"
-							width="300"
-							v-bind="attrs"
-							v-on="on"
-						></v-card>
-					</div>
-				</template>
-
-				<v-list>
-					<v-list-item
-						v-for="(item, index) in items"
-						:key="index"
-					>
-						<v-list-item-title>{{ item.title }}</v-list-item-title>
-					</v-list-item>
-				</v-list>
-			</v-menu>
-		</v-row>
-	</v-container>
+	<PuzzleList />
 </template>
 
 <script>
+	import PuzzleList from '@/components/PuzzleList'
+
 	export default{
+		components: {
+			PuzzleList,
+		},
 		data(){
-			return {
-				showMenu: false,
-				items: [
-        { title: 'Click Me' },
-        { title: 'Click Me' },
-        { title: 'Click Me' },
-        { title: 'Click Me 2' },
-      	],
-			}
 		}
 	}
 </script>
-
-<style>
-	.title{
-		color: black;
-		text-align: center;
-		padding-bottom: 300px;
-	}
-
-</style>
